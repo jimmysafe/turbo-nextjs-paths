0d3893ddbe0b6884541dfeebc39f871d8f3614f4
Gianmarco Ciaffardini <ciaffardini.g@gmail.com>
2024-12-07 11:36:05 +0100

    init5

--- apps/web-store/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div >
-      <h1>WEB STORE</h1>
-    </div>
-  );
-}
--- apps/web-store/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  basePath: '/store',
-};
-
-export default nextConfig;
