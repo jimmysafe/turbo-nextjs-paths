98ba8aa45d4f0e4cd6b0b879a4f120d8b266084f
Gianmarco Ciaffardini <ciaffardini.g@gmail.com>
2024-12-07 14:05:29 +0100

    feat: ui apps

--- apps/main/app/page.tsx
+++ apps/main/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@repo/ui/components/ui/button';
+
 export default function Home() {
   return (
     <div>
       <h1>MAIN APP</h1>
+      <Button>Ciao</Button>
     </div>
   );
 }
--- apps/org/app/org/page.tsx
+++ apps/org/app/org/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@repo/ui/components/ui/button';
 export default function Home() {
   return (
     <div>
       <h1>WEB ORGANIZATION</h1>
+      <Button>ORG</Button>
     </div>
   );
 }
